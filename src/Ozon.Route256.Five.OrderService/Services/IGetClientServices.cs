7509d48730e3b23cc3343a12e5c7cf8d963ffae2
Наталья Буранова <natasha.buranova@gmail.com>
2023-03-20 11:09:11 +0400

    [CSHW-3]: fix

--- src/Ozon.Route256.Five.OrderService/Services/IGetClientServices.cs
+++ src/Ozon.Route256.Five.OrderService/Services/IGetClientServices.cs
@@ -5,12 +5,12 @@ namespace Ozon.Route256.Five.OrderService.Services;
 public interface IGetClientServices
 {
     /// <summary>
-    /// Получение всех клиентом из сервиса клиентов
+    /// Получение всех клиентов из сервиса клиентов
     /// </summary>
     Task<List<ClientResponse>> GetClientsAsync(CancellationToken token);
 
     /// <summary>
-    /// Получения информации о клиента по ИД из сервиса клиентов
+    /// Получения информации о клиенте по ИД из сервиса клиентов
     /// </summary>
     Task<ClientResponse> GetClientAsync(int customerId, CancellationToken token);
 }
