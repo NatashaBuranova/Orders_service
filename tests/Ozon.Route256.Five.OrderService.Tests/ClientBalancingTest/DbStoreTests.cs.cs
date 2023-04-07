1b5bceb44d0ddaba9e4efe4a8ef4ed5b9ccd2596
Наталья Буранова <natasha.buranova@gmail.com>
2023-04-07 12:16:31 +0400

    [CSHW-6]: fix test

--- tests/Ozon.Route256.Five.OrderService.Tests/ClientBalancingTest/DbStoreTests.cs.cs
+++ tests/Ozon.Route256.Five.OrderService.Tests/ClientBalancingTest/DbStoreTests.cs.cs
@@ -11,25 +11,25 @@ public class DbStoreTests
         _dbStore = new DbStore();
     }
 
-    //[Fact]
-    //public async Task GetNextEndpointAsync_Successful()
-    //{
-    //    //Arrange
-    //    var dbEndpoints = new[]
-    //        {
-    //        new DbEndpoint("testHost1", DbReplicaType.Master),
-    //        new DbEndpoint("testHost2", DbReplicaType.Master),
-    //        };
-    //    await _dbStore.UpdateEndpointsAsync(dbEndpoints);
+    [Fact]
+    public async Task GetNextEndpointAsync_Successful()
+    {
+        //Arrange
+        var dbEndpoints = new[]
+            {
+            new DbEndpoint("testHost1", DbReplicaType.Master, new int[]{4}),
+            new DbEndpoint("testHost2", DbReplicaType.Master, new int[]{4}),
+            };
+        await _dbStore.UpdateEndpointsAsync(dbEndpoints);
 
-    //    //Act
-    //    var resultFirst = await _dbStore.GetNextEndpointAsync();
-    //    var resultSecond = await _dbStore.GetNextEndpointAsync();
-    //    var resultThird = await _dbStore.GetNextEndpointAsync();
+        //Act
+        var resultFirst = await _dbStore.GetNextEndpointAsync();
+        var resultSecond = await _dbStore.GetNextEndpointAsync();
+        var resultThird = await _dbStore.GetNextEndpointAsync();
 
-    //    //Assert
-    //    Assert.Equal(dbEndpoints[0].HostAndPort,resultFirst.HostAndPort);
-    //    Assert.Equal(dbEndpoints[1].HostAndPort, resultSecond.HostAndPort);
-    //    Assert.Equal(dbEndpoints[0].HostAndPort, resultThird.HostAndPort);
-    //}
+        //Assert
+        Assert.Equal(dbEndpoints[0].HostAndPort, resultFirst.HostAndPort);
+        Assert.Equal(dbEndpoints[1].HostAndPort, resultSecond.HostAndPort);
+        Assert.Equal(dbEndpoints[0].HostAndPort, resultThird.HostAndPort);
+    }
 }
